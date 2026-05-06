68ddca3427a4acd26760164df561f6516ffa6658
Aly-Hassan-Ahmed <alialialghamrawy@gmail.com>
2026-05-06 05:20:25 +0300

    task One

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,8 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("lol");
+            double small_cost = 25;
+            double large_cost = 35;
+            double cost = 0;
+            double tax = 0;
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.Write("Number of small carpets: ");
+            int small_num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of large carpets: ");
+            int large_num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Price per small room: $25");
+            Console.WriteLine("Price per large room: $35");
+           
+            if (small_num > 0)
+            {
+                cost += small_num * small_cost;
+            }
+            if (large_num > 0)
+            {
+                cost += large_num * large_cost;   
+            }
+            tax +=  cost * 0.06;
+            Console.WriteLine($"Cost : ${cost}");
+            Console.WriteLine($"Tax : ${tax}");
+            Console.WriteLine("===============================");
+            Console.WriteLine($"Total estimate: ${cost+tax}");
+            Console.WriteLine("This estimate is valid for 30 days");
         }
     }
 }
